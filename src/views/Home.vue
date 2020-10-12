0b26a93a8ce4bce241700afb4def64cf87bf6eec
Illia Matviichuk <mtillia98@gmail.com>
2020-10-12 19:30:55 -0400

    First steps

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h2>Home</h2>
+<!--        <router-link to="/about">About</router-link>-->
+    </div>
+</template>
+
+<script>
+// import { reactive, onMounted, computed } from 'vue'
+//
+// export default {
+//     props: {
+//         title: String
+//     },
+//     setup (props, { emit }) {
+//         const state = reactive({
+//             username: '',
+//             password: '',
+//             lowerCaseUsername: computed(() => state.username.toLowerCase())
+//         })
+//
+//         onMounted(() => {
+//             console.log('title: ' + props.title)
+//         })
+//
+//         const login = () => {
+//             emit('login', {
+//                 username: state.username,
+//                 password: state.password
+//             })
+//         }
+//
+//         return {
+//             login,
+//             state
+//         }
+//     }
+// }
+</script>
